1f030fb02753725c6509722baebc2cb2297cdbb0
Aneesh Jose <31209870+aneeshjose@users.noreply.github.com>
2021-05-20 15:46:30 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/slideintro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.slideintro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/introduction.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class IntroductionScreen extends StatefulWidget {
+  @override
+  _IntroductionScreenState createState() => _IntroductionScreenState();
+}
+
+class _IntroductionScreenState extends State<IntroductionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
